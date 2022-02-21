c5998a84897f2659e9b01730ebdc69d8304c71c9
Mathias Quintero <me@quintero.io>
2022-02-21 20:55:08 +0000

    making delay in try again strategy mandatory

--- Sources/Sync/SwiftUI/Reconnection/TryAgainReconnectionStrategy.swift
+++ Sources/Sync/SwiftUI/Reconnection/TryAgainReconnectionStrategy.swift
@@ -2,24 +2,19 @@
 import Foundation
 
 extension ReconnectionStrategy where Self == TryAgainReconnectionStrategy {
-    public static var tryAgain: ReconnectionStrategy {
-        return TryAgainReconnectionStrategy()
-    }
-
     public static func tryAgain(delay: TimeInterval) -> ReconnectionStrategy {
         return TryAgainReconnectionStrategy(delay: delay)
     }
 }
 
 public struct TryAgainReconnectionStrategy: ReconnectionStrategy {
-    private let delay: TimeInterval?
+    private let delay: TimeInterval
 
-    public init(delay: TimeInterval? = nil) {
+    public init(delay: TimeInterval) {
         self.delay = delay
     }
 
     public func maybeReconnect() async -> ReconnectionDecision {
-        guard let delay = delay else { return .attemptToReconnect }
         do {
             try await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
             return .attemptToReconnect
